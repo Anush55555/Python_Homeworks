6802df1f13db6a775a8948cf72911b8045a73a2b
Anush55 <annmanukyan555@gmail.com>
2023-10-08 22:22:20 +0400

    Number Classification

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-a = (float)(input("Enter the first value: "))
-b = (float)(input("Enter the second value: ") )
-c = input("Enter the operation(+,-,*,/): ")
+def classify_numbers(numbers):
+    even_numbers = []
+    odd_numbers = []
 
-if c=='+':
-    print(a+b)
-elif c=='-':
-    print(a-b)
-elif c=='*':
-    print(a*b)
-else:
-    print(a/b)
+    for num in numbers:
+        if num % 2 == 0:
+            even_numbers.append(num)
+        else:
+            odd_numbers.append(num)
+
+    return even_numbers, odd_numbers
+
+def main():
+    userInput = input("Enter a list of numbers separated by spaces: ")
+
+    numbers = [int(x) for x in userInput.split()]
+
+    even_numbers, odd_numbers = classify_numbers(numbers)
+    print("List of even numbers:", even_numbers)
+    print("List of odd numbers:", odd_numbers)
+
+if __name__ == "__main__":
+    main()
